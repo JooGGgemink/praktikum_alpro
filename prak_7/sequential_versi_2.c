fe2b6855fb17cf449bc46077f0afbcaf84ad0293
JooGGgemink <163154311+JooGGgemink@users.noreply.github.com>
2024-06-05 20:21:24 +0700

    Add files via upload

---
+++ prak_7/sequential_versi_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int sequentialSearch2(int arr[], int size, int x) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == x) {
+            return i;  // Return the index where the element is found
+        }
+    }
+    return -1;  // Element not found
+}
+
+int main() {
+    int T[] = {19, 1, 28, 5, 20, 15, 52, 13, 16, 29, 71, 65, 10, 18, 87, 5, 90, 35, 7, 11};
+    int size = sizeof(T) / sizeof(T[0]);
+
+    int x1 = 15;
+    int result1 = sequentialSearch2(T, size, x1);
+    if (result1 != -1) {
+        printf("Element %d found at index %d\n", x1, result1);
+    } else {
+        printf("Element %d not found\n", x1);
+    }
+
+    int x2 = 60;
+    int result2 = sequentialSearch2(T, size, x2);
+    if (result2 != -1) {
+        printf("Element %d found at index %d\n", x2, result2);
+    } else {
+        printf("Element %d not found\n", x2);
+    }
+
+    return 0;
+}
